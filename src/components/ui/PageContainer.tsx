a6e1cd3486bb82c459edf261d86cb1c8b6149f69
Mushfikur Rahman <59982815+mushfikurr@users.noreply.github.com>
2025-02-03 01:08:57 +0000

    Update PageContainer.tsx
    Reduce padding on the sides of page (for mobile)

--- src/components/ui/PageContainer.tsx
+++ src/components/ui/PageContainer.tsx
@@ -10,7 +10,7 @@ export function PageContainer({
       className={cn("min-h-screen flex flex-col h-full", className)}
       {...props}
     >
-      <div className="flex flex-col items-center grow pt-24 py-4 gap-10 px-14 text-text-100 max-sm:pt-20">
+      <div className="flex flex-col items-center grow pt-24 py-4 gap-10 px-8 text-text-100 max-sm:pt-20">
         {children}
       </div>
     </div>
