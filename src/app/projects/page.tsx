bae99c2d8fd13ef9d2ba5479a0c4eca12c5648b4
Mushfikur Rahman <mushfikur0@gmail.com>
2025-02-03 19:10:34 +0000

    Quick fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,5 @@
 import { Projects } from "@/components/Projects";
-import { QUERIES } from "@/lib/db/queries";
 
 export default async function ProjectsPage() {
-  const posts = await QUERIES.getPostsByType("projects");
-  return <Projects posts={posts} />;
+  return <Projects />;
 }
