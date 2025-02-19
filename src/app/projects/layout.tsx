715e1f8171311da276495e399edabaec2a1c2f81
Mushfikur Rahman <mushfikur0@gmail.com>
2025-02-19 14:55:26 +0000

    Use 100dvh instead of h-screen

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <div
       className={cn(
-        "max-w-prose w-full h-full mx-auto text-text-100 py-12 px-8 pb-24 gap-8 min-h-screen"
+        "max-w-prose w-full h-full mx-auto text-text-100 py-12 px-8 pb-24 gap-8 min-h-dvh"
       )}
     >
       <Breadcrumb parent="projects" />
--- src/components/ui/PageContainer.tsx
+++ src/components/ui/PageContainer.tsx
@@ -6,10 +6,7 @@ export function PageContainer({
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
   return (
-    <div
-      className={cn("min-h-screen flex flex-col h-full", className)}
-      {...props}
-    >
+    <div className={cn("min-h-dvh flex flex-col h-full", className)} {...props}>
       <div className="flex flex-col items-center grow pt-24 py-4 gap-10 px-8 text-text-100 max-sm:pt-20">
         {children}
       </div>
